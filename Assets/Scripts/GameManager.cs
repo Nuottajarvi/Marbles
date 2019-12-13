5ad032bbbffdfbc70b57a0e2094853cd651a7fed
Armi Korhonen <c5koar00@students.oamk.fi>
2019-12-13 19:59:07 +0200

    Scriptintynkiä ja testiscene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int amountOfPlayers;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //asetetaan pelaajan valitsema näppäin
+    public void AssignKeysToPlayers(Player player, KeyCode keyCode, int keyNumber)
+    {
+        //keyNumber 1 on playerKey_1, keyNumber 2 on playerKey_2
+        if (keyNumber == 1)
+        {
+            player.playerKey_1 = keyCode;            
+        } else if (keyNumber == 2)
+        {
+            player.playerKey_2 = keyCode;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public KeyCode playerKey_1;
+    public KeyCode playerKey_2;
+}
