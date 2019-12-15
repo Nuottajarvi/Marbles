97ba60c7f01aa7c331ee22d9c2bf5fb58bc9f10d
nuottajarvi <peetu.nuottajarvi@gmail.com>
2019-12-15 13:09:43 +0200

    Roulette scripts

---
+++ Assets/Accelerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerator : MonoBehaviour
+{
+    public float force;
+    void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if(rb)
+        {
+            rb.AddForce(transform.forward * force, ForceMode.Acceleration);
+        }
+    }
+}
